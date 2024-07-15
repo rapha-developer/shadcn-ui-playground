fed739bd0931f85e42af53d0a75ed0a97776cb74
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-15 14:21:15 -0300

    feat: import browserRouter and make the application routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { Products } from "./pages/products"
 
 function App() {
-
   return (
-    <>
-      <h1 className="">Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
