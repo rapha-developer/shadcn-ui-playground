23f44a01c842d8db2e93591fafa72c01f7946d98
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-15 12:59:31 -0300

    refactor: import useSearchParams to get id and name (from url params), to use to fill the forms used by user

--- src/components/product-filters.tsx
+++ src/components/product-filters.tsx
@@ -4,6 +4,7 @@ import { Input } from "./ui/input";
 import { useForm } from "react-hook-form";
 import { z } from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useSearchParams } from "react-router-dom";
 
 const productsFilterSchema = z.object({
   id: z.string(),
@@ -11,13 +12,40 @@ const productsFilterSchema = z.object({
 })
 
 type ProductsFiltersSchema = z.infer<typeof productsFilterSchema>
+
 export function ProductsFilters() {
+
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const id = searchParams.get('id')
+  const name = searchParams.get('name')
+
   const { register, handleSubmit } = useForm<ProductsFiltersSchema>({
-    resolver: zodResolver(productsFilterSchema)
+    resolver: zodResolver(productsFilterSchema),
+    values: {
+      id: id ?? '',
+      name: name ?? ''
+    }
   })
 
-  function handleFilterProducts(data: ProductsFiltersSchema) {
-    console.log(data)
+  function handleFilterProducts({ id, name }: ProductsFiltersSchema) {
+    setSearchParams((state) => {
+      if(id) {
+        state.set('id', id)
+      } else {
+        state.delete('id')
+      }
+      return state
+    })
+
+    setSearchParams((state) => {
+      if(name) {
+        state.set('name', name)
+      } else {
+        state.delete('name')
+      }
+      return state
+    })
   }
     return (
       <form onSubmit={handleSubmit(handleFilterProducts)} className="flex items-center gap-2">
