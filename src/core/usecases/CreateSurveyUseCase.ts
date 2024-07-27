3446481df95140b0e0ee1d4c1f09ed6a36aef29b
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-27 17:38:46 -0300

    feat: export (create survey) use case. Responsible for create the survey whenever called

---
+++ src/core/usecases/CreateSurveyUseCase.ts
@@ -0,0 +1,23 @@
+import { Survey } from "../entities/Survey"
+
+
+export class CreateSurveyUseCase {
+    constructor(
+        private title: string,
+        private resume: string,
+        private category: {
+            text:  "CSS" | "componentes" | "bloco" | "guias",
+            color: '#3aa9fd' | '#ff2ed9' | '#227dff' | '#6A4EE9'
+        },
+        private id?: string
+    ) {}
+
+    execute(): Survey {
+        return new Survey({
+            id: this?.id, 
+            title: this.title,
+            resume: this.resume,
+            category: this.category,
+        })
+    }
+}
