bed6d636b0f13c72d74b5dbd7716b9081667b7a1
Rapha Developer <contatosraphaelmoreira@gmail.com>
2024-07-27 17:50:06 -0300

    type: export SurveyProps type. Reponsible for typing attributes used by survey entity

---
+++ src/types/survey.ts
@@ -0,0 +1,9 @@
+export type SurveyProps = {
+    id?: string
+    title: string
+    resume: string
+    category: {
+        text:  "CSS" | "componentes" | "bloco" | "guias"
+        color: '#3aa9fd' | '#ff2ed9' | '#227dff' | '#6A4EE9'
+    }
+} 
